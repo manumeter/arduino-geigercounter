6fb317b187836e0fcb7f5e0ac7b8d08584517c44
Manuel <manuel@deadlock>
2017-05-03 22:53:49 +0200

    web-stats

---
+++ stats.php
@@ -0,0 +1,32 @@
+<html>
+<body>
+<pre>
+M4011 Geiger Tube
+-----------------
+Tin oxide Cathode, Coaxial cylindrical thin shell structure
+(Wall density 50±10cg/cm2), Application of pulse type halogen
+tube application temperature: -40°C~55°C
+Could be used for: γRay 20mR/h~120mR/h  
+and β Ray in range 100~1800 ChangingIndex/minutes·CM2 soft β Ray
+(Both beta and gamma radiation detetion)
+Working Voltage: 380-450V 
+Working Current: 0,015-0,02mA
+Sensivity to Gamma Radiation: 0.1MeV
+Own Background: 0,2 Pulses/s
+Length:  88mm
+Diameter: 10mm
+Conversion Factor: approx. 150
+
+Conversion
+----------
+x = Counts per 10 Minutes
+
+(x-120)/1.5      = nSv/h
+(x-120)/1.5*8.76 = μSv/y
+
+Date       Time     x
+---------- -------- ---
+<?=file_get_contents('stats.log')?>
+</pre>
+</body>
+</html>
